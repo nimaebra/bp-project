992768fb6cc94504f3c3e2c122ce10507b97cd1b
Nima Ebrazeh <nimaebra75@gmail.com>
2021-02-28 21:47:04 +0330

    Practice model added!

--- panel/admin.py
+++ panel/admin.py
@@ -1,1 +1,7 @@
 from django.contrib import admin
+
+# Models
+from .models import Practice
+
+# Register Practive model in admin
+admin.site.register(Practice)
--- panel/models.py
+++ panel/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Practice(models.Model):
+    title = models.CharField(max_length=200)
+    comment = models.CharField(max_length=500)
+    deadline = models.DateTimeField(null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
