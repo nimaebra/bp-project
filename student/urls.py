58161aaec35a3668679e6b2c61a837379169019b
Nima Ebrazeh <nimaebra75@gmail.com>
2021-03-01 23:09:09 +0330

    student templates added!

--- student/urls.py
+++ student/urls.py
@@ -3,5 +3,13 @@ from django.urls import path
 from .views import Practices
 
 urlpatterns = [
-    path('practices', Practices.as_view(), name='practices')
+    # path('login', Login.as_view(), name='login'),
+    # path('dashboard', Dashboard.as_view(), name='dashboard'),
+    path('practices', Practices.as_view(), name='practices-list'),
+    # path('practices/create', PracticeCreate.as_view(), name='practice-create'),
+    # path('practices/<int:pk>', PracticesAnswers.as_view(),
+    #      name='practices-answers'),
+    # path('videos', VideosList.as_view(), name='videos-list'),
+    # path('videos/<int:pk>', VideosDetail.as_view(), name='videos-detail'),
+    # path('videos/create', VideoCreate.as_view(), name='video-create'),
 ]
